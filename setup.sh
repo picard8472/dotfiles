fdf506de5a175aa1266624a3ea58673e4329182c
Linus Kendall <me@linuskendall.com>
2024-06-18 23:19:54 +0200

    updated setup.sh
    added some more comments

--- setup.sh
+++ setup.sh
@@ -5,13 +5,14 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-
+# Start tailscale
+# @TODO add check
 tailscale up --accept-routes --ssh --authkey $TAILSCALE_KEY
 
+# vault settings
 export VAULT_USERNAME=linuskendall
 
 # ssh
-
 mkdir -p $HOME/.ssh/
 ln -s .ssh $HOME/.ssh/
 
@@ -19,3 +20,4 @@ ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -P ""
 
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
+
